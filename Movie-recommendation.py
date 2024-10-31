ee6d05d6cea2357a71257d088978ff6fa590fe0b
shrish Pattewar <147394036+Shrishp@users.noreply.github.com>
2024-10-31 17:11:50 +0000

    recommed changes

--- Movie-recommendation.py
+++ Movie-recommendation.py
@@ -344,11 +344,24 @@ similarity[2]
 # In[247]:
 
 
+# def recommend(movie):
+#     movie_index = new_df[new_df['title'] == movie].index[0]
+#     distances = similarity[movie_index]
+#     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    
+#     for i in movies_list:
+#         print(new_df.iloc[i[0]].title)
 def recommend(movie):
+    # Check if the movie exists in the DataFrame
+    if movie not in new_df['title'].values:
+        print(f"Movie '{movie}' not found in the database.")
+        return
+    
     movie_index = new_df[new_df['title'] == movie].index[0]
     distances = similarity[movie_index]
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
     
+    # Print recommended movie titles
     for i in movies_list:
         print(new_df.iloc[i[0]].title)
 
